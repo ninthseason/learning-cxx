8a50b1982de6699cbfedcf9577daa8dc579fa483
Kl1nge5 <ninthseason@qq.com>
2026-01-31 14:35:14 +0800

    finish 0-4

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -2,6 +2,9 @@
 
 // READ: `static` 关键字 <https://zh.cppreference.com/w/cpp/language/storage_duration>
 // THINK: 这个函数的两个 `static` 各自的作用是什么？
+// My answer：static 使其 specify 的 declarator 1. 拥有静态储存期 2. 内部链接
+// func 声明在全局作用域，本身拥有静态储存期，static 作用在于使其不可被外部链接
+// static_ 声明在函数作用域，本身就不可外部链接，static 作用在于使其拥有静态储存期
 static int func(int param) {
     static int static_ = param;
     // std::cout << "static_ = " << static_ << std::endl;
@@ -10,10 +13,10 @@ static int func(int param) {
 
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 5, "static variable value incorrect");
+    ASSERT(func(4) == 6, "static variable value incorrect");
+    ASSERT(func(3) == 7, "static variable value incorrect");
+    ASSERT(func(2) == 8, "static variable value incorrect");
+    ASSERT(func(1) == 9, "static variable value incorrect");
     return 0;
 }
